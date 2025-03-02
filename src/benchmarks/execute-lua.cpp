cbd4aa0b4a6b23b27381562e72acaa26e518663a
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2025-03-02 12:41:36 +0300

    Add the 'cpplua' library

--- src/benchmarks/execute-lua.cpp
+++ src/benchmarks/execute-lua.cpp
@@ -1,23 +1,38 @@
 #include <miet/lambda-executor/handlers/execute-lambda.hpp>
 
-#include <cstdint>
-#include <iterator>
-#include <string_view>
-
 #include <benchmark/benchmark.h>
 #include <userver/engine/run_standalone.hpp>
 
-/**
- * @todo Write a benchmark for Lua execution */
+#include <LuaCpp/LuaCpp.hpp>
+
 void ExecuteLuaBenchmark(benchmark::State& state) {
   userver::engine::RunStandalone([&] {
-    constexpr std::string_view kNames[] = {"userver", "is", "awesome", "!"};
+    constexpr std::uint64_t kArgs[] = {11, 12, 13, 14, 15};
     std::uint64_t i = 0;
 
+    LuaCpp::LuaContext ctx;
+    ctx.CompileString("benchmark", R"(
+      function factorial(n)
+        if n == 0 or n == 1
+          then return 1
+        end
+        return n * factorial(n - 1)
+      end
+
+      res = factorial(N)
+      sum = 0
+      for i = 0, res, 1 do
+        sum = sum + i
+      end
+    )");
+    const auto NVar = std::make_shared<LuaCpp::Engine::LuaTNumber>(0);
+    ctx.AddGlobalVariable("N", NVar);
+
     for (auto _ : state) {
-      const auto name = kNames[i++ % std::size(kNames)];
-      auto result = std::string(name) + "_stub";
-      benchmark::DoNotOptimize(result);
+      const auto N = kArgs[i % std::size(kArgs)];
+      NVar->setValue(N);
+      ctx.Run("benchmark");
+      i++;
     }
   });
 }
